be2cac839a5f8336dec07f2a16722b8dec7df192
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2023-11-30 13:53:56 -0800

    Fix file paths

--- php/process_borrowing_form.php
+++ php/process_borrowing_form.php
@@ -3,8 +3,8 @@
 $registrationCode = $_POST['registrationCodeBorrowing'];
 $isbn = $_POST['isbn'];
 
-$jsonFile = file_get_contents("data/borrowingData.json");
-$data = json_decode($jsonFile, true);
+$BorrowingJsonFile = file_get_contents("../data/borrowingData.json");
+$data = json_decode($BorrowingJsonFile, true);
 
 $newData = array(
     "registration_code" => $registrationCode,
@@ -14,22 +14,27 @@ $newData = array(
 $data[] = $newData;
 
 $jsonData = json_encode($data);
-file_put_contents("data/borrowingData.json", $jsonData);
+file_put_contents("../data/borrowingData.json", $jsonData);
 
-$bookJsonFile = file_get_contents("data/bookData.json");
+// Access bookData.json
+$bookJsonFile = file_get_contents("../data/bookData.json");
 $books = json_decode($bookJsonFile, true);
 
+// Search for the book with matching ISBN
 foreach ($books as &$book) {
     if ($book['isbn'] == $isbn) {
+        // Subtract 1 from the availables value
         $newAvailables = strval(intval($book['availables']) - 1);
         $book['availables'] = $newAvailables;
         break;
     }
 }
 
+// Save the updated bookData.json
 $bookJsonData = json_encode($books);
-file_put_contents("data/bookData.json", $bookJsonData);
+file_put_contents("../data/bookData.json", $bookJsonData);
 
-header('Location: /index.html');
+// Redirect back to index page
+header('Location: ../index.html');
 exit;
 ?>
